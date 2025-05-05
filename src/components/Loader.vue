347f3f6708cd017612471cbaab66caeca97db5f4
Marsel Gabbasov <marsel13_98@mail.ru>
2025-05-06 00:07:15 +0800

    added loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="loader__wrap">
+        <div class="loader"></div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .loader {
+        border: 8px solid #f3f3f3; /* светлый фон */
+        border-top: 8px solid #3498db; /* цвет верха (анимации) */
+        border-radius: 50%;
+        width: 60px;
+        height: 60px;
+        animation: spin 1s linear infinite;
+        &__wrap{
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+        }
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+</style>
